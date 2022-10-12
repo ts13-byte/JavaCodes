4a40a768047d4e7e72bf9a7459e6c347ef1451e0
ts13-byte <87604053+ts13-byte@users.noreply.github.com>
2022-10-12 15:31:49 +0530

    Update least_key_greater_than_key.java

--- least_key_greater_than_key.java
+++ least_key_greater_than_key.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
 
-public class least_key_greater_than_key {
+public class LeastkeyGreaterThanKey {
     public static void main(String[] args) {
 
         HashMap<Integer, String> hs
