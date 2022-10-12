f807fcdde2032e888e2d598da841fdcf78dbcfbf
ts13-byte <87604053+ts13-byte@users.noreply.github.com>
2022-10-12 13:17:49 +0530

    Create comparing_sets.java

---
+++ comparing_sets.java
@@ -0,0 +1,22 @@
+package work;
+
+import java.util.HashSet;
+
+public class comparing_two_sets {
+    public static void main(String[] args) {
+
+        HashSet<Integer> h1=new HashSet<>();
+        HashSet<Integer> h2=new HashSet<>();
+        h1.add(1);
+        h1.add(3);
+        h1.add(4);
+        h1.add(5);
+
+        h2.add(3);
+        h2.add(7);
+        h2.add(1);
+
+        h2.retainAll(h1);
+        System.out.println(h2);
+    }
+}
