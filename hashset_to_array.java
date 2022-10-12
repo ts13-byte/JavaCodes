864f2692dc69439875dc1ccef83217381f7b2841
ts13-byte <87604053+ts13-byte@users.noreply.github.com>
2022-10-12 13:24:47 +0530

    Create hashset_to_array.java

---
+++ hashset_to_array.java
@@ -0,0 +1,18 @@
+package work;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class set_to_array {
+    public static void main(String[] args) {
+        HashSet<String> hs=new HashSet<>();
+        hs.add("sara");
+        hs.add("saraa");
+        hs.add("saraaa");
+        hs.add("saraaaa");
+       // String arr[]=new String[hs.size()];
+
+        String[] names=hs.stream().toArray(String[]::new);
+        System.out.println(Arrays.toString(names));
+    }
+}
