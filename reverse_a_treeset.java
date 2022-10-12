9b9332adac7aace904a80032300b7039c769ec08
ts13-byte <87604053+ts13-byte@users.noreply.github.com>
2022-10-12 13:23:34 +0530

    Create reverse_a_treeset.java

---
+++ reverse_a_treeset.java
@@ -0,0 +1,17 @@
+package work;
+
+import java.util.*;
+
+public class reversing {
+    public static void main(String[] args) {
+        TreeSet<Integer> ts=new TreeSet<>();
+
+        ts.add(3);
+        ts.add(4);
+        ts.add(6);
+        ts.add(1);
+        System.out.println(ts);
+        Set<Integer> s=ts.descendingSet();
+        System.out.println(s);
+    }
+}
